b1e976ff0b6277d4d937021f5b586255357a02fc
MuriMan <shahanaabrar2008@gmail.com>
2025-02-11 15:52:20 +0600

    changes

--- sketch.js
+++ sketch.js
@@ -6,13 +6,41 @@ function preload() {
 
 function setup() {
     canvas = createCanvas(600, 400);
-    canvas.parent('canvas-container'); // If used inside an HTML file
-    background(220);
-
-	image(us, 0, 0);
-	describe("yay");
+    canvas.parent('canvas-container');
 }
 
+var leftX = 100;
+var rightX = 300;
+
+var leftMove = false;
+var rightMove = false;
+
 function draw() {
-    // Your drawing code here
+    background(220);
+
+	imageMode(CENTER);
+	image(us, 300, 200);
+
+	fill(color(255, 0, 0));
+
+	rect(leftX, 50, 200, 300);
+	rect(rightX, 50, 200, 300);
+
+	if (leftMove) {
+		leftX = lerp(leftX, -200, 0.5);
+	}
+
+	if (rightMove) {
+		rightX = lerp(rightX, 600, 0.5);
+	}
+}
+
+function mouseClicked() {
+	if (mouseX > 100 && mouseX < 300) {
+		leftMove = true;
+	}
+
+	if (mouseX > 300 && mouseX < 500) {
+		rightMove = true;
+	}
 }
