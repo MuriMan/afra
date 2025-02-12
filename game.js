9a354fc94abe6cc0d31bcaad932c68f28ee58836
MuriMan <shahanaabrar2008@gmail.com>
2025-02-12 21:30:07 +0600

    add flowers

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 let canvas;
 
+var x1 = 100;
+var angle = 0.0;
+
 function setup() {
-    canvas = createCanvas(600, 400);
-    canvas.parent('canvas-container');
+	canvas = createCanvas(600, 400);
+	canvas.parent('canvas-container');
 
 	x = floor(random() * 600);
 	y = floor(random() * 400);
@@ -13,24 +16,50 @@ var y = 0;
 
 var collected = 0;
 function draw() {
-    background(220);
+	background(220);
 
-	fill(255, 0, 0);
+	if (collected >= 7) {
+		stroke(0);
+		fill(0, 239, 255);
+		circle(325, 243, 50);
+		circle(325, 157, 50);
+		circle(275, 243, 50);
+		circle(275, 157, 50);
+		circle(250, 200, 50);
+		circle(350, 200, 50);
 
-	noStroke();
-	circle(x + 10, y + 10, 20);
-	circle(x + 30, y + 10, 20);
-	beginShape();
+		fill(255);
+		circle(width/2, height/2, 100);
+
+		for (let i = 5; i > 0; i--) {
+			let rad = i * 10;
+			if (i % 2 == 0) {
+				fill(0, 239, 255);
+				circle(width/2, height/2, rad * 2);
+			}
+			else {
+				fill(247, 255, 0);
+				circle(width/2, height/2, rad * 2);
+			}
+
+		}
+	
+		document.getElementById("nextPage").style.display = "block";
+	} else {
+		fill(255, 0, 0);
+
+		noStroke();
+		circle(x + 10, y + 10, 20);
+		circle(x + 30, y + 10, 20);
+		beginShape();
 		vertex(x + 1.34, y + 15);
 		vertex(x + 20, y + 10);
 		vertex(x + 38.7, y + 15);
 		vertex(x + 20, y + 30);
-	endShape();
+		endShape();
 
-	if (!(collected >= 7)) {
 		document.getElementById("nextPage").style.display = "none";
-	} else {
-		document.getElementById("nextPage").style.display = "block";
+		document.getElementById("flower").style.display = "none";
 	}
 
 	document.getElementById("heartCount").innerText = "Collected: " + collected;
